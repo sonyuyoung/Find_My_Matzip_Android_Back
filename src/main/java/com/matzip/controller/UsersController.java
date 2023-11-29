39014b0d969e98a19b8da2dc7751309f98bdcdf0
jurwon <susorrow@naver.com>
2023-11-29 10:18:05 +0900

    회원가입시 토큰 적용되는 문제 해결

--- src/main/java/com/matzip/controller/UsersController.java
+++ src/main/java/com/matzip/controller/UsersController.java
@@ -20,10 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/users")
