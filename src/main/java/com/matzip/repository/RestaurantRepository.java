3383f02784fd19642af6fa962a06a0ed0c59216a
jurwon <susorrow@naver.com>
2024-01-04 15:17:11 +0900

    식당 검색 오류 해결

--- src/main/java/com/matzip/repository/RestaurantRepository.java
+++ src/main/java/com/matzip/repository/RestaurantRepository.java
@@ -32,13 +32,12 @@ List<Object[]> findTopNByOrderByAvgScoreDesc(Pageable pageable);
              "GROUP BY r.resId, r.res_name")
      List<Object[]> findAllByOrderByAvgScoreDesc(Pageable pageable);
 
-     //검색된 식당 조회
-     @Query("SELECT r.resId, r.res_name, r.res_district, r.res_lat, r.res_lng, r.res_address, r.res_phone, r.operate_time, r.res_menu, r.res_thumbnail, r.res_intro,  AVG(b.score) as avgScore " +
+     //검색된 식당 조회 ->유사도 순으로 내림차순 정렬
+     @Query("SELECT r.resId, r.res_name, r.res_district, r.res_lat, r.res_lng, r.res_address, r.res_phone, r.operate_time, r.res_menu, r.res_thumbnail, r.res_intro,  COALESCE(AVG(b.score), 0.0) as avgScore " +
              "FROM Restaurant r " +
-             "JOIN r.boards b " +
+             "LEFT JOIN r.boards b " +
              "WHERE r.res_name like %:text%  or r.res_menu like %:text% or r.res_address like %:text% "+
-             "GROUP BY r.resId, r.res_name "+
-             "ORDER BY avgScore DESC")
+             "GROUP BY r.resId, r.res_name ")
      List<Object[]> findSearchByOrderByAvgScoreDesc(Pageable pageable, @Param("text") String text);
 
      @Query("SELECT AVG(b.score) FROM Restaurant r JOIN r.boards b WHERE r.resId = :resId")
